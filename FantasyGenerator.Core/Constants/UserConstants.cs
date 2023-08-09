d7c40ba595ccd92226bc573a0d840c29dee950a3
redfoxbg <redfoxbg@gmail.com>
2023-08-09 23:35:52 +0300

    Add Role for app users: Admin, User, Guest and Author

---
+++ FantasyGenerator.Core/Constants/UserConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace FantasyGenerator.Core.Constants
+{
+    public static class UserConstants
+    {
+        public static class Roles 
+        {
+            public const string Administrator = "Administrator";
+
+            public const string Author = "Author, User";
+
+            public const string User = "User";
+
+            public const string Guest = "Guest";
+        }
+    }
+}
---
+++ FantasyGenerator/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using FantasyGenerator.Core.Constants;
+
+
+namespace FantasyGenerator.Controllers
+{
+    public class UserController : BaseController
+    {
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UserController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager; 
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CreateRole()
+        {
+            //await _roleManager.CreateAsync(new IdentityRole { Name = UserConstants.Roles.Administrator});
+            //await _roleManager.CreateAsync(new IdentityRole { Name = UserConstants.Roles.Author });
+            //await _roleManager.CreateAsync(new IdentityRole { Name = UserConstants.Roles.User });
+            //await _roleManager.CreateAsync(new IdentityRole { Name = UserConstants.Roles.Guest });
+            return Ok();
+        }
+
+    }
+}
